3d3b391b68c873559e5f22145397bb44b8d09705
Varshiha <varshihaa@icloud.com>
2025-08-29 11:01:35 -0400

    I did the sunset using the code. I added slowMoveVertical(250);

--- Picture.java
+++ Picture.java
@@ -16,7 +16,6 @@ public class Picture
     private Triangle roof;
     private Circle sun;
     private boolean drawn;
-    private Circle sun2;
 
     /**
      * Constructor for objects of class Picture
@@ -27,7 +26,6 @@ public class Picture
         window = new Square();
         roof = new Triangle();  
         sun = new Circle();
-        sun2 = new Circle();
         drawn = false;
     }
 
@@ -53,17 +51,14 @@ public class Picture
             roof.moveVertical(-60);
             roof.makeVisible();
     
-            sun.changeColor("blue");
+            sun.changeColor("yellow");
             sun.moveHorizontal(100);
             sun.moveVertical(-40);
             sun.changeSize(80);
             sun.makeVisible();
+            sun.slowMoveVertical(250);
             
-            sun2.changeColor("yellow");
-            sun2.moveHorizontal(-100);
-            sun2.moveVertical(-60);
-            sun2.changeSize(80);
-            sun2.makeVisible();
+            
             
             drawn = true;
         }
@@ -78,7 +73,7 @@ public class Picture
         window.changeColor("white");
         roof.changeColor("black");
         sun.changeColor("black");
-        sun2.changeColor("black");
+        
     }
 
     /**
@@ -89,7 +84,6 @@ public class Picture
         wall.changeColor("red");
         window.changeColor("black");
         roof.changeColor("green");
-        sun.changeColor("blue");
-        sun2.changeColor("yellow");
+        sun.changeColor("sun");
     }
 }

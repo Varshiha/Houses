cbbb4b81a0b6e5e97effcbbd485a561040507b3d
Varshiha <varshihaa@icloud.com>
2025-08-29 10:33:45 -0400

    I added a second sun using the code.

--- Picture.java
+++ Picture.java
@@ -16,6 +16,7 @@ public class Picture
     private Triangle roof;
     private Circle sun;
     private boolean drawn;
+    private Circle sun2;
 
     /**
      * Constructor for objects of class Picture
@@ -26,6 +27,7 @@ public class Picture
         window = new Square();
         roof = new Triangle();  
         sun = new Circle();
+        sun2 = new Circle();
         drawn = false;
     }
 
@@ -51,11 +53,18 @@ public class Picture
             roof.moveVertical(-60);
             roof.makeVisible();
     
-            sun.changeColor("yellow");
+            sun.changeColor("blue");
             sun.moveHorizontal(100);
             sun.moveVertical(-40);
             sun.changeSize(80);
             sun.makeVisible();
+            
+            sun2.changeColor("yellow");
+            sun2.moveHorizontal(-100);
+            sun2.moveVertical(-60);
+            sun2.changeSize(80);
+            sun2.makeVisible();
+            
             drawn = true;
         }
     }
@@ -69,6 +78,7 @@ public class Picture
         window.changeColor("white");
         roof.changeColor("black");
         sun.changeColor("black");
+        sun2.changeColor("black");
     }
 
     /**
@@ -79,6 +89,7 @@ public class Picture
         wall.changeColor("red");
         window.changeColor("black");
         roof.changeColor("green");
-        sun.changeColor("yellow");
+        sun.changeColor("blue");
+        sun2.changeColor("yellow");
     }
 }
